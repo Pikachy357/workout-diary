2aadfb3c76647ebbe1e20fd1f9fe2f4a7266d250
Shiphter <dyatlov-daniil@mail.ru>
2026-06-02 14:04:23 +0300

    start

--- src/ExerciseSet.java
+++ src/ExerciseSet.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ExerciseSet {
+    private double weight;
+    private int reps;
+    private short difficulty;
+    private String comment;
+
+    public ExerciseSet(double weight, int reps, short difficulty, String comment) {
+        this.weight = weight;
+        this.reps = reps;
+        this.difficulty = difficulty;
+        this.comment = comment;
+    }
+
 }
